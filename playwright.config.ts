fd52d193b291495c57b25f8e435485f7dfaa3388
Sergii <34283525+Saleks13@users.noreply.github.com>
2024-12-05 12:37:46 -0500

    Setting up reporting.

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['lsit'],
+    ['list'],
     ['html', { outputFolder: 'playwright-report', open: 'never' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
