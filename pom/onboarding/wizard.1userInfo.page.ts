217f04c47a74e21305565cdf9f63993491087387
Sergii <34283525+Saleks13@users.noreply.github.com>
2024-12-05 10:49:43 -0500

    Add GitHub Actions workflow for Playwright tests

--- pom/onboarding/wizard.1userInfo.page.ts
+++ pom/onboarding/wizard.1userInfo.page.ts
@@ -49,7 +49,7 @@ export default class Wizard1UserInfoPage {
     // Language selector
     public async selectLanguage() {
         const locale = process.env.LOCALE || "EN";
-        console.log("LOCALE = " + locale);
+        //console.log("LOCALE = " + locale);
         if (!locale) {
             throw new Error(`Locale is not defined in the environment variables.`);
         }
