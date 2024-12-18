2813cc1cebbf38b4611d06c33092c9950ae54716
Sergii <34283525+Saleks13@users.noreply.github.com>
2024-12-18 17:04:18 -0500

    Screenshots naming optimized.

--- tests/no-auth/signIn.spec.ts
+++ tests/no-auth/signIn.spec.ts
@@ -14,3 +14,19 @@ test("#SI-0001 - Sign in with single organization user. @smoke", async ({ signIn
     await overviewGeneral.assertPageTitleVisible();
 
 })
+
+
+test("#SI-0002 - Other test", async ({ signInPage, overviewGeneral }) => {
+
+    // go to sign in page
+    await signInPage.goto();
+    // set existing username email 
+    await signInPage.fillUsername(process.env.USERS_USERSINGLEORG_EMAIL || "");
+    // set correct password
+    await signInPage.fillPassword(process.env.USERS_USERSINGLEORG_PASSWORD || "");
+    // click sign in button
+    await signInPage.clickLoginButton();
+    // check overview header visible 
+    await overviewGeneral.assertPageTitleVisible();
+
+})
